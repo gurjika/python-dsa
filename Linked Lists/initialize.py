cd3314ef627c3b489e7f3183b47c69c9707a10fd
Luka <lgurjidze@gmail.com>
2024-07-04 15:08:40 +0400

    linked list: prepend, pop_first get, set, insert

--- Linked Lists/initialize.py
+++ Linked Lists/initialize.py
@@ -27,7 +27,19 @@ class LinkedList:
         return True
 
     def prepend(self, value):
-        pass
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+        return True
+
+        
 
     def insert(self, index, value):
         pass
@@ -62,18 +74,86 @@ class LinkedList:
                 return popped.value
             
             temp = temp.next
-            
+    
+    def pop_first(self):
+
+        temp = self.head
+
+        if self.head is None:
+            return None
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return temp
+        
+        self.length -= 1
+        self.head = self.head.next
+        temp.next = None
+        return temp
+
+    def get(self, index):
+ 
+        if self.length <= index or index < 0:
+            return None
+        
+        temp = self.head
+
+        # while temp is not None:
+        #     if counter == index:
+        #         return temp.value
+        #     temp = temp.next
+        #     counter += 1
+
+        for _ in range(index):
+            temp = temp.next
+
+        return temp
+    
+    def set(self, index, value):
+
+        temp = self.get(index)
+
+        if temp:
+            temp.value = value
+            return True
+        
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+
+        if index == 0:
+            self.prepend(value)
+            return True
+        
+        if index == self.length:
+            return self.append(value)
+         
+        temp = self.get(index - 1)
+        new_node = Node(value)
+        next_node = temp.next
+        temp.next = new_node
+        new_node.next = next_node
+        self.length += 1
+        return True
+    
+
 
 linked_list = LinkedList(4)
-linked_list.pop()
+linked_list.append(2)
+linked_list.insert(2, 5)
 
-linked_list.print_list()
-print()
-print(f'popped item {linked_list.pop()}')
-
-print()
+# linked_list.pop_first()
 linked_list.print_list()
 print()
 
-print(f'tail: {linked_list.tail}')
-print(f'head: {linked_list.head}')
+print()
+linked_list.print_list()
+print()
+
+print(f'tail: {linked_list.tail.value}')
+print(f'head: {linked_list.head.value}')
+print(linked_list.get(0))
