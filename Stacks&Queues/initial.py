a1b3dc7662c903c8a252147c8eea5ac2407bec25
Luka <lgurjidze@gmail.com>
2024-10-01 19:22:43 +0400

    queue enqueue dequeue

--- Stacks&Queues/initial.py
+++ Stacks&Queues/initial.py
@@ -35,11 +35,65 @@ class Stack:
         self.top = temp.next
         temp.next = None
         self.height -= 1
-        return temp
+        return temp.value
 
 stack = Stack(5)
 stack.push(2)
 stack.push(3)
 stack.print_stack()
-stack.pop()
+print(stack.pop(), '\n')
 stack.print_stack()
+print()
+
+class Queue:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+
+    def enqueue(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+
+        else:
+            self.last.next = new_node
+            self.last = new_node
+
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+
+        temp = self.first
+
+        if self.length == 1:
+            self.first = None
+            self.last = None 
+
+        else:
+            self.first = temp.next
+            temp.next = None
+        
+        self.length
+        return temp.value
+
+    def print_queue(self):
+
+        temp = self.first
+
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+queue = Queue(3)
+queue.enqueue(2)
+queue.enqueue(5)
+queue.print_queue()
+queue.dequeue()
+queue.print_queue()
