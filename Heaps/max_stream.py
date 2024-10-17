0650c5e2affac0ceb5450cabec420609bf87086d
Luka <lgurjidze@gmail.com>
2024-10-17 17:00:31 +0400

    max stream

---
+++ Heaps/max_stream.py
@@ -0,0 +1,39 @@
+from initial import MaxHeap
+
+
+
+def stream_max(nums):
+    heap = MaxHeap()
+
+    result_list = []
+
+
+    for num in nums:
+        heap.insert(num)
+
+        result_list.append(heap.heap[0])
+
+
+    return result_list
+
+
+
+
+test_cases = [
+    ([], []),
+    ([1], [1]),
+    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
+    ([1, 2, 2, 1, 3, 3, 3, 2, 2], [1, 2, 2, 2, 3, 3, 3, 3, 3]),
+    ([-1, -2, -3, -4, -5], [-1, -1, -1, -1, -1])
+]
+
+for i, (nums, expected) in enumerate(test_cases):
+    result = stream_max(nums)
+    print(f'\nTest {i+1}')
+    print(f'Input: {nums}')
+    print(f'Expected Output: {expected}')
+    print(f'Actual Output: {result}')
+    if result == expected:
+        print('Status: Passed')
+    else:
+        print('Status: Failed')
