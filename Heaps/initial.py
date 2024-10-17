7ef3198dde630241a387a7460c46819f0dd0bf90
Luka <lgurjidze@gmail.com>
2024-10-17 16:18:47 +0400

    heap insert

--- Heaps/initial.py
+++ Heaps/initial.py
@@ -16,4 +16,14 @@ class MaxHeap:
         self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
 
 
-    
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current != 0 and self.heap[current] > self._parent(current):
+            new_index = self._parent(current)
+            self._swap(current, new_index)
+            current = new_index
+            
+
+            
