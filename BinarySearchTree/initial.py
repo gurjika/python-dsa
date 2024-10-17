f2455a3a617120f57d97e7bc5ab63d54bfa0f00c
Luka <lgurjidze@gmail.com>
2024-10-17 17:53:42 +0400

    bst recursive search

--- BinarySearchTree/initial.py
+++ BinarySearchTree/initial.py
@@ -57,6 +57,23 @@ class BinarySearchTree:
 
 
         return False
+    
+
+    def __r_contains(self, current_node, value):
+        if current_node == None:
+            return False
+        
+        if value == current_node.value:
+            return True
+        
+        if value < current_node.value:
+            return self.__r_contains(current_node.left, value)
+        
+        elif value > current_node.value:
+            return self.__r_contains(current_node.right, value)
+        
+    def r_contains(self, value):
+        return self.__r_contains(self.root, value)
 
 bst = BinarySearchTree()
 
@@ -69,3 +86,5 @@ bst.insert(6)
 
 
 print(bst.contains(4))
+
+print(bst.r_contains(6))
